933f86222ceca6f915227205d0cabc12d644dfce
sakshisuthar-cca <sakshisuthar@cca.edu>
2025-07-24 18:32:05 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -248,7 +248,8 @@ export default function AgentComponent() {
       fontWeight: 400,
       lineHeight: "14px",
       margin: 0,
-      maxWidth: "242px",
+      minWidth: "fit-content",
+      maxWidth: "250px",
       marginRight: "12px",
       wordBreak: "break-word",
       textAlign: "left", // Ensures proper alignment
